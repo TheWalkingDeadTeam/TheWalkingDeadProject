76435d6dda65886d01d788da70686f1299d0b844
Kryvonis <kryvonis.artem@gmail.com>
2016-04-25 22:25:37 +0300

    REST

--- web/resources/js/profile.js
+++ web/resources/js/profile.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
 });
 
 function getDataForProfile() {
-    $.ajax ({
-        method: 'GET',
-        url: 'resources/js/json/getProfile.json'
-        // url: 'resources/js/json/getEmptyProfile.json'
-    })
-    .done( function(data) {
-        handleProfileResponse(data);
-    });
+    $.ajax({
+            method: 'GET',
+            url: 'resources/js/json/getProfile.json'
+            // url: 'resources/js/json/getEmptyProfile.json'
+        })
+        .done(function (data) {
+            handleProfileResponse(data);
+        });
 }
 
 function formSubmitHandler(e) {
@@ -34,38 +34,38 @@ function formSubmitHandler(e) {
     var multiplyQuestions = $('#multiplyQuestions');
     var singleQuestions = $('#singleQuestions');
 
-    programingLangList.find('input').each(function (i,item) {
+    programingLangList.find('input').each(function (i, item) {
         requestData.programingLanguages.push({
             id: $(item).attr('name'),
             assesment: $(item).val()
         });
     });
 
-    singleQuestions.find('input:checked').each(function (i,item) {
+    singleQuestions.find('input:checked').each(function (i, item) {
         requestData.singleQuestions[$(item).attr('name')] = $(item).val();
     });
 
-    multiplyQuestions.find('input:checked').each(function (i,item) {
-        if( requestData.multiplyQuestions[$(item).attr('name')]) {
+    multiplyQuestions.find('input:checked').each(function (i, item) {
+        if (requestData.multiplyQuestions[$(item).attr('name')]) {
             requestData.multiplyQuestions[$(item).attr('name')].push($(item).val())
         } else {
             requestData.multiplyQuestions[$(item).attr('name')] = [$(item).val()];
         }
     });
 
-    $.ajax ({
+    $.ajax({
             method: 'POST',
             contentType: "application/json",
             url: 'profile',
             data: JSON.stringify(requestData)
         })
-        .done( function(data) {
+        .done(function (data) {
             console.log(data);
         });
 }
 
 function handleProfileResponse(data) {
-    if(data.name) {
+    if (data.name) {
         renderProfile(data);
     } else {
         renderEmptyProfile(data);
@@ -84,9 +84,9 @@ function renderEmptyProfile(data) {
 
 function renderProfile(data) {
     var name = $('input[name=name]');
-    var surname= $('input[name=surname]');
-    var tel= $('input[name=tel]');
-    var email= $('input[name=email]');
+    var surname = $('input[name=surname]');
+    var tel = $('input[name=tel]');
+    var email = $('input[name=email]');
     var programingLangList = $('#programingLangList');
     var multiplyQuestions = $('#multiplyQuestions');
     var singleQuestions = $('#singleQuestions');
@@ -106,40 +106,40 @@ function renderProfile(data) {
     appendSingleItemsToList(singleQuestions, data.singleQuestions);
 }
 
-function appendOptionToSelect(element,options) {
-    options.values.forEach( function (item) {
-        var isSelected =  options.selected === item  ? 'selected' : '';
+function appendOptionToSelect(element, options) {
+    options.values.forEach(function (item) {
+        var isSelected = options.selected === item ? 'selected' : '';
 
-        $(element).append($('<option '+ isSelected +'>' + item + '</option>'));
+        $(element).append($('<option ' + isSelected + '>' + item + '</option>'));
     })
 }
 
-function appendItemsToList(element,options) {
-    options.forEach( function (item) {
+function appendItemsToList(element, options) {
+    options.forEach(function (item) {
         $(element).append($('<li><label>' + item.name + '<input name="' + item.id + '" type="number" min="0" max="10" value="' + item.assesment + '"></label></li>'))
     })
 }
 
-function appendMultipluItemsToList(element,options) {
-    options.forEach( function (item) {
-        var questionsItem =  $('<li><span>' + item.title + '</span></li>');
+function appendMultipluItemsToList(element, options) {
+    options.forEach(function (item) {
+        var questionsItem = $('<li><span>' + item.title + '</span></li>');
 
-        item.allValues.forEach( function (qItem) {
-            var isCheked =  ~item.checkedValues.indexOf(qItem) ? 'checked' : '';
-            var questionItem =  $('<span>' + qItem +': <input type="checkbox" name="' + item.questionId +'" value="' + qItem + '"' + isCheked +'></span> ');
+        item.allValues.forEach(function (qItem) {
+            var isCheked = ~item.checkedValues.indexOf(qItem) ? 'checked' : '';
+            var questionItem = $('<span>' + qItem + ': <input type="checkbox" name="' + item.questionId + '" value="' + qItem + '"' + isCheked + '></span> ');
             questionsItem.append(questionItem);
         });
         $(element).append(questionsItem);
     })
 }
 
-function appendSingleItemsToList(element,options) {
-    options.forEach( function (item) {
-        var questionsItem =  $('<li><span>' + item.title + '</span></li>');
+function appendSingleItemsToList(element, options) {
+    options.forEach(function (item) {
+        var questionsItem = $('<li><span>' + item.title + '</span></li>');
 
-        item.allValues.forEach( function (qItem) {
-            var isCheked =  item.selectedValue === qItem ? 'checked' : '';
-            var questionItem =  $('<span>' + qItem +': <input type="radio" name="' + item.questionId +'" value="' + qItem + '"' + isCheked +'></span> ');
+        item.allValues.forEach(function (qItem) {
+            var isCheked = item.selectedValue === qItem ? 'checked' : '';
+            var questionItem = $('<span>' + qItem + ': <input type="radio" name="' + item.questionId + '" value="' + qItem + '"' + isCheked + '></span> ');
             questionsItem.append(questionItem);
         });
         $(element).append(questionsItem);
