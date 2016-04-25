be809ab4a62b611d7e338177ae7f967caf364fe6
shylovd <202ktlbgtljq11>
2016-04-25 22:47:17 +0300

    working version, maybe

---
+++ classes/artifacts/TheWalkingDeadProject_war_exploded/WEB-INF/jsp/admin.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Pavel
+  Date: 18.04.2016
+  Time: 20:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>ADMIN</h1>
+</body>
+</html>
---
+++ classes/artifacts/TheWalkingDeadProject_war_exploded/resources/js/register.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Pavel on 25.04.2016.
+ */
+$(document).ready(function () {
+    $("#registerButton").click(function () {
+        event.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: '/register',
+            dataType: 'json',
+            contentType: "application/json",
+            data: JSON.stringify({
+                name: $('#name').val(),
+                email: $('#email').val(),
+                password: $('#password').val()
+            }),
+            success: function (response) {
+                window.location.href = response.redirect; 
+
+            }
+        });
+    });
+});
