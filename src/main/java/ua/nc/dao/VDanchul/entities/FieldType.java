91a99a5d274ed3f1b6ef9ce88b539f9ba90edb43
vdanchul <rangargst85@gmail.com>
2016-04-27 01:30:57 +0300

    Fixed some wrong types and added protected setID.

--- src/main/java/ua/nc/dao/VDanchul/entities/FieldType.java
+++ src/main/java/ua/nc/dao/VDanchul/entities/FieldType.java
@@ -4,11 +4,13 @@ package ua.nc.dao.VDanchul.entities;
  * Created by Rangar on 24.04.2016.
  */
 public class FieldType implements Identified<Integer>{
-    public int ID;
+    private int ID;
     public String name;
 
     @Override
     public Integer getID() {
         return ID;
     }
+
+    protected void setID(int id) { this.ID = id;  }
 }
