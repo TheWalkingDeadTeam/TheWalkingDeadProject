6f18e146e89466dd91d97028200f29102acc6d53
vdanchul <rangargst85@gmail.com>
2016-04-27 14:39:50 +0300

    trying to commit my changes

---
+++ src/main/java/ua/nc/dao/VDanchul/entities/Profile.java
@@ -0,0 +1,34 @@
+package ua.nc.dao.VDanchul.entities;
+
+import java.util.HashMap;
+
+/**
+ * Created by Rangar on 25.04.2016.
+ */
+
+
+/*public class Profile {
+
+    private HashMap<Field, FieldValue> _fields = new HashMap<>();
+
+    public Profile(CES ces, User user) {
+        // load fields and fieldValues
+    "select * from field_value fv join application ap on fv.Application_ID = ap.Application_ID
+    join user us on us.User_ID = ap.User_ID
+    where ap.CES_ID = (твое выражение)"
+    }
+
+    public ListType<Field> getFields() {
+    }
+
+    public FieldValue getFieldValue(Field field) {
+    }
+
+    public void setField(int fieldId, String value) {
+    }
+    // For each field type (String, Date etc.)
+    public void save() {
+        // Insert/update of table fieldvalue
+    }
+
+}*/
