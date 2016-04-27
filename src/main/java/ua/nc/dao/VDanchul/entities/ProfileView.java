7ff9817161d518a911c0b6b777066fa5edf9d864
vdanchul <rangargst85@gmail.com>
2016-04-27 14:39:50 +0300

    trying to commit my changes

---
+++ src/main/java/ua/nc/dao/VDanchul/entities/ProfileView.java
@@ -0,0 +1,29 @@
+package ua.nc.dao.VDanchul.entities;
+
+/**
+ * Created by Rangar on 26.04.2016.
+ */
+/*public class ProfileView {
+    private Profile
+    public ProfileView(Profile profile) {}
+
+    public JSON getProfileData() {
+        JSON result = new JSON();
+
+        for (Field field in this.profile.getFields()) {
+            FieldValue fieldValue = this.profile.getFieldValue(field);
+
+            JSON fieldResult = new JSON();
+            fieldResult.set("id", field.id);
+            fieldResult.set("type", );
+            // for other fields too
+
+        }
+    }
+    public void setProfileData(JSON input) {
+        for (JSON fieldJson in input.getArray()){
+            this.profile.setField(fieldJson.get("id"), fieldJson.get("value"));
+        }
+        this.profile.save();
+    }
+}*/
