f2327be24a5ac03f3e66c0312fb6166e4ce0d50c
vdanchul <rangargst85@gmail.com>
2016-05-02 02:44:04 +0300

    Special finder getApplicationByUserCES() added.

---
+++ src/main/java/ua/nc/dao/ApplicationDAO.java
@@ -0,0 +1,12 @@
+package ua.nc.dao;
+
+import ua.nc.entity.Application;
+
+import java.util.List;
+
+/**
+ * Created by Rangar on 01.05.2016.
+ */
+public interface ApplicationDAO extends GenericDAO<Application, Integer> {
+    Application getApplicationByUserCES(Integer user_id, Integer ces_id);
+}
