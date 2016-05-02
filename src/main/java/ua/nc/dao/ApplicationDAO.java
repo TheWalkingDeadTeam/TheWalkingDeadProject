6d3451ae95feccbc2c395f16ff0f1672c7e63679
vdanchul <rangargst85@gmail.com>
2016-05-02 17:35:32 +0300

    throwing exception added.

--- src/main/java/ua/nc/dao/ApplicationDAO.java
+++ src/main/java/ua/nc/dao/ApplicationDAO.java
@@ -1,5 +1,6 @@
 package ua.nc.dao;
 
+import ua.nc.dao.exception.DAOException;
 import ua.nc.entity.Application;
 
 import java.util.List;
@@ -8,5 +9,5 @@ import java.util.List;
  * Created by Rangar on 01.05.2016.
  */
 public interface ApplicationDAO extends GenericDAO<Application, Integer> {
-    Application getApplicationByUserCES(Integer user_id, Integer ces_id);
+    Application getApplicationByUserCES(Integer user_id, Integer ces_id) throws DAOException;
 }
