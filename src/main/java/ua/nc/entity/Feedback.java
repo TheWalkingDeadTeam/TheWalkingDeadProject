48b09735d7250ab9855d1572406ca82a2e277420
vdanchul <rangargst85@gmail.com>
2016-05-02 13:04:57 +0300

    feedbackClass added.

---
+++ src/main/java/ua/nc/entity/Feedback.java
@@ -0,0 +1,50 @@
+package ua.nc.entity;
+
+/**
+ * Created by Rangar on 02.05.2016.
+ */
+public class Feedback implements Identified<Integer> {
+    private int feedbackID;
+    private int score;
+    private String comment;
+    private int interviewerID;
+
+    @Override
+    public Integer getID() {
+        return null;
+    }
+
+    protected void setID(int id){
+        this.feedbackID = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getInterviewerID() {
+        return interviewerID;
+    }
+
+    public void setInterviewerID(int interviewerID) {
+        this.interviewerID = interviewerID;
+    }
+
+    public Feedback(int score, String comment, int interviewerID){
+        this.score = score;
+        this.comment = comment;
+        this.interviewerID = interviewerID;
+    }
+}
