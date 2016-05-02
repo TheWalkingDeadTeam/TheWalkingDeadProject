b88136fdf9d0d685a408562c46e87c3da2a72c30
vdanchul <rangargst85@gmail.com>
2016-05-02 13:49:21 +0300

    exceptions added.

--- src/main/java/ua/nc/service/ProfileService.java
+++ src/main/java/ua/nc/service/ProfileService.java
@@ -1,15 +1,17 @@
 package ua.nc.service;
 
 
+import ua.nc.dao.exception.DAOException;
 import ua.nc.entity.profile.FieldValue;
 import ua.nc.entity.profile.Profile;
 
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  * Created by Pavel on 28.04.2016.
  */
 public interface ProfileService {
-    Profile getProfile(UserDetailsImpl userDetails, int cesID);
-    void setProfile(UserDetailsImpl userDetails, int cesID, Profile profile);
+    Profile getProfile(UserDetailsImpl userDetails, int cesID) throws DAOException;
+    void setProfile(UserDetailsImpl userDetails, int cesID, Profile profile) throws DAOException, SQLException;
 }
