9d1d24ca9e2cebdd6a5f1d22957018319703d747
TanasiichukYaroslav <tanasiichuk9247@gmail.com>
2016-05-03 13:36:34 +0200

    change method validateMultipleChoiceField, add pattern change method validateNotMultipleChoiceField

--- src/main/java/ua/nc/validator/ProfileValidator.java
+++ src/main/java/ua/nc/validator/ProfileValidator.java
@@ -7,6 +7,8 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Pavel on 28.04.2016.
@@ -23,27 +25,57 @@ public class ProfileValidator implements Validator {
         while (profileFieldIterator.hasNext()) {
             ValidationError validationError = null;
             profileField = profileFieldIterator.next();
+
             if (profileField.isMultipleChoice()) {
                 validationError = validateMultipleChoiceField(profileField, 1, 3);
-            }else {
+            } else {
                 validationError = validateNotMultipleChoiceField(profileField);
             }
             if (validationError != null) {
-                    errors.add(validationError);
+                errors.add(validationError);
                 }
-            }
+        }
             return errors;
         }
     private ValidationError validateNotMultipleChoiceField(ProfileField profileField){
         ValidationError validationError = null;
+        if (!profileField.getValues().get(0).getValue().isEmpty()) {
+            if(profileField.getFieldType().equals("Value_Date")){
+                String datePattern = new String("(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\\\d\\\\d)");
+                Pattern pattern =  Pattern.compile(datePattern);
+                Matcher matcher = pattern.matcher(profileField.getValues().get(0).getValue());
+                if(!matcher.matches()){
+                    validationError = new ValidationError(profileField.getFieldName(),"Please, enter date in correct format");
+                }
+            }
+            if(profileField.getFieldType().equals("Value_Double")){
+                String doublePattern = new String("([0-9]+.?[0-9]+)");
+                Pattern pattern =  Pattern.compile(doublePattern);
+                Matcher matcher = pattern.matcher(profileField.getValues().get(0).getValue());
+                if(!matcher.matches()){
+                    validationError = new ValidationError(profileField.getFieldName(),"Please, enter number");
+                }
+            }
+            if(profileField.getFieldType().equals("List_Value")){
+                if(profileField.getValues().size()!=1){
+                    validationError = new ValidationError(profileField.getFieldName(),"You should choose one fields ");
+                }
 
+            }
+        } else {
+            validationError = new ValidationError(profileField.getFieldName(), "Field shouldn't be empty");
+        }
         return validationError;
     }
     private ValidationError validateMultipleChoiceField (ProfileField profileField, int min, int max){
         ValidationError validationError = null;
-        int size = profileField.getValues().size();
-        if (size<min || size>max ) {
-        validationError = new ValidationError(profileField.getFieldName(),"You should choose from 2 to 4 values");
+        if (!profileField.getValues().isEmpty()) {
+            int size = profileField.getValues().size();
+            if (size<min || size>max ) {
+                validationError = new ValidationError(profileField.getFieldName(),"You should choose from "+min+" to "+max+" values");
+                }
+        } else {
+            validationError = new ValidationError(profileField.getFieldName(), "You should choose some fields ");
         }
         return validationError;
     }
