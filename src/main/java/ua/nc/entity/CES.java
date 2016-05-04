ce5d709d444a72729bf5692e13dec7cea3037415
vdanchul <rangargst85@gmail.com>
2016-05-04 15:28:15 +0300

    CES added.

--- src/main/java/ua/nc/entity/CES.java
+++ src/main/java/ua/nc/entity/CES.java
@@ -1,10 +1,34 @@
 package ua.nc.entity;
 
+import java.util.Calendar;
+import java.util.List;
+
 /**
- * Created by Pavel on 03.05.2016.
+ * Created by Ермоленко on 01.05.2016.
  */
 public class CES {
     private Integer id;
+    private Integer year;
+    private Calendar startRegistrationDate;
+    private Calendar endRegistrationDate;
+    private Calendar startInterviewingDate;
+    private Calendar endInterviewingDate;
+    private Integer quota;
+    private Integer reminders;
+    private Integer statusId;
+    private Integer interviewTimeForPerson;
+    private Integer interviewTimeForDay;
+
+    public CES(Integer year, Calendar startRegistrationDate, Integer quota, Integer reminders, Integer statusId,
+               Integer interviewTimeForPerson, Integer interviewTimeForDay){
+        this.year = year;
+        this.startRegistrationDate = startRegistrationDate;
+        this.quota = quota;
+        this.reminders = reminders;
+        this.statusId = statusId;
+        this.interviewTimeForPerson = interviewTimeForPerson;
+        this.interviewTimeForDay = interviewTimeForDay;
+    }
 
     public Integer getId() {
         return id;
@@ -13,4 +37,85 @@ public class CES {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public Integer getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(Integer statusId) {
+        this.statusId = statusId;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Calendar getStartRegistrationDate() {
+        return startRegistrationDate;
+    }
+
+    public void setStartRegistrationDate(Calendar startRegistrationDate) {
+        this.startRegistrationDate = startRegistrationDate;
+    }
+
+    public Calendar getEndRegistrationDate() {
+        return endRegistrationDate;
+    }
+
+    public void setEndRegistrationDate(Calendar endRegistrationDate) {
+        this.endRegistrationDate = endRegistrationDate;
+    }
+
+    public Calendar getStartInterviewingDate() {
+        return startInterviewingDate;
+    }
+
+    public void setStartInterviewingDate(Calendar startInterviewingDate) {
+        this.startInterviewingDate = startInterviewingDate;
+    }
+
+    public Calendar getEndInterviewingDate() {
+        return endInterviewingDate;
+    }
+
+    public void setEndInterviewingDate(Calendar endInterviewingDate) {
+        this.endInterviewingDate = endInterviewingDate;
+    }
+
+    public Integer getQuota() {
+        return quota;
+    }
+
+    public void setQuota(Integer quota) {
+        this.quota = quota;
+    }
+
+    public Integer getReminders() {
+        return reminders;
+    }
+
+    public void setReminders(Integer reminders) {
+        this.reminders = reminders;
+    }
+
+    public Integer getInterviewTimeForPerson() {
+        return interviewTimeForPerson;
+    }
+
+    public void setInterviewTimeForPerson(Integer interviewTimeForPerson) {
+        this.interviewTimeForPerson = interviewTimeForPerson;
+    }
+
+    public Integer getInterviewTimeForDay() {
+        return interviewTimeForDay;
+    }
+
+    public void setInterviewTimeForDay(Integer interviewTimeForDay) {
+        this.interviewTimeForDay = interviewTimeForDay;
+    }
 }
+
