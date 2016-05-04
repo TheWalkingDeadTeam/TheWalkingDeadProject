c7877517f4525ba53040bf5ac39256691df77975
vdanchul <rangargst85@gmail.com>
2016-05-04 15:27:57 +0300

    CESStatus added.

---
+++ src/main/java/ua/nc/entity/CESStatus.java
@@ -0,0 +1,31 @@
+package ua.nc.entity;
+
+/**
+ * Created by Ермоленко on 01.05.2016.
+ */
+public class CESStatus implements Identified<Integer> {
+
+    private int id;
+    private String name;
+
+    public CESStatus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    protected void setID(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public Integer getID() {
+        return id;
+    }
+}
