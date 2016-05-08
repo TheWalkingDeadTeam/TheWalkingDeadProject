e59f216d2b9ad0db039a519df7876820b1766406
vdanchul <rangargst85@gmail.com>
2016-05-08 23:49:37 +0300

    The way our Application table data looks like.

---
+++ src/main/java/ua/nc/entity/profile/RowValue.java
@@ -0,0 +1,12 @@
+package ua.nc.entity.profile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Rangar on 08.05.2016.
+ */
+public class RowValue {
+    public int applicationId;
+    public Map<String, Object> fields = new HashMap<>();
+}
