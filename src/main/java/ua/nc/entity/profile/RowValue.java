f46a2ffbc0180fd164ed8a7b022b475396fcfc5d
vdanchul <rangargst85@gmail.com>
2016-05-09 13:44:22 +0300

    applicationId --> userId

--- src/main/java/ua/nc/entity/profile/RowValue.java
+++ src/main/java/ua/nc/entity/profile/RowValue.java
@@ -7,6 +7,6 @@ import java.util.Map;
  * Created by Rangar on 08.05.2016.
  */
 public class RowValue {
-    public int applicationId;
+    public int userId;
     public Map<String, Object> fields = new HashMap<>();
 }
