11a9e85ae742853d5738ad0baf0b779ab57e2281
vdanchul <rangargst85@gmail.com>
2016-05-09 19:30:19 +0300

    public StudentData getApplicationTable(cesId) public StudentData getApplicationTable(cesId, limit, offset) public StudentData getApplicationTable(cesId, limit, offset, orderBy)

--- src/main/java/ua/nc/dao/postgresql/PostgreApplicationTableDAO.java
+++ src/main/java/ua/nc/dao/postgresql/PostgreApplicationTableDAO.java
@@ -61,23 +61,57 @@ public class PostgreApplicationTableDAO {
         return MessageFormat.format(template, fieldId);
     }
 
-    public StudentData getApplications(Integer cesId) throws DAOException {
+    private static final String baseQuery = (
+            "SELECT " +
+                    "system_user.system_user_id, " +
+                    "system_user.name, " +
+                    "system_user.surname, {0} " +
+                    "FROM public.application " +
+                    "JOIN public.system_user ON application.system_user_id = system_user.system_user_id " +
+                    "JOIN public.course_enrollment_session ON course_enrollment_session.ces_id = application.ces_id " +
+                    "JOIN public.ces_field ON ces_field.ces_id = course_enrollment_session.ces_id " +
+                    "JOIN public.field ON field.field_id = ces_field.field_id " +
+                    "JOIN public.field_type ON field.field_type_id = field_type.field_type_id " +
+                    "JOIN public.field_value ON field_value.field_id = field.field_id " +
+                    "LEFT JOIN public.list_value ON field_value.list_value_id = list_value.list_value_id " +
+                    "WHERE course_enrollment_session.ces_id = ? " +
+                    "GROUP BY system_user.system_user_id "
+    );
+
+    private static final String orderByQuery = " ORDER BY {0} ";
+    private static final String limitOffsetQuery = " LIMIT {0}, OFFSET {1} ";
+
+    public StudentData getApplicationsTable(Integer cesId, Integer limit, Integer offset) throws DAOException {
         List<FieldData> fieldData = getFieldIds(cesId);
-        String baseQuery = (
-                "SELECT " +
-                "system_user.system_user_id, " +
-                "system_user.name, " +
-                "system_user.surname, {0} " +
-                "FROM public.application " +
-                "JOIN public.system_user ON application.system_user_id = system_user.system_user_id " +
-                "JOIN public.course_enrollment_session ON course_enrollment_session.ces_id = application.ces_id " +
-                "JOIN public.ces_field ON ces_field.ces_id = course_enrollment_session.ces_id " +
-                "JOIN public.field ON field.field_id = ces_field.field_id " +
-                "JOIN public.field_type ON field.field_type_id = field_type.field_type_id " +
-                "JOIN public.field_value ON field_value.field_id = field.field_id " +
-                "LEFT JOIN public.list_value ON field_value.list_value_id = list_value.list_value_id " +
-                "GROUP BY system_user.system_user_id"
-        );
+        String baseQuery = buildBaseFullQuery(fieldData);
+        String fullQuery = baseQuery + MessageFormat.format(limitOffsetQuery, limit, offset);
+        return getApplications(cesId, fullQuery, fieldData);
+    }
+
+    public StudentData getApplicationsTable(Integer cesId, Integer limit, Integer offset, String orderBy) throws DAOException {
+        List<FieldData> fieldData = getFieldIds(cesId);
+        String baseQuery = buildBaseFullQuery(fieldData);
+        String almostFullQuery = baseQuery + MessageFormat.format(limitOffsetQuery, limit, offset);
+        String fullQuery = almostFullQuery + MessageFormat.format(orderByQuery, orderBy);
+        return getApplications(cesId, fullQuery, fieldData);
+    }
+
+    public StudentData getApplicationsTable(Integer cesId) throws DAOException {
+        List<FieldData> fieldData = getFieldIds(cesId);
+        return getApplications(cesId, buildBaseFullQuery(fieldData), fieldData);
+    }
+
+    private StudentData getApplications(Integer cesId, String fullQuery, List<FieldData> fieldData) throws DAOException {
+        try (PreparedStatement statement = this.connection.prepareStatement(fullQuery)) {
+            statement.setInt(1, cesId);
+            ResultSet rs = statement.executeQuery();
+            return parseResultSet(rs, fieldData);
+        } catch (Exception e) {
+            throw new DAOException(e);
+        }
+    }
+
+    private String buildBaseFullQuery(List<FieldData> fieldData) throws DAOException {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < fieldData.size() - 1; i++) {
             builder.append(subQuery(fieldData.get(i).id));
@@ -85,29 +119,27 @@ public class PostgreApplicationTableDAO {
         }
         builder.append(subQuery(fieldData.get(fieldData.size() - 1).id));
         String sub = builder.toString();
-        String fullQuery = MessageFormat.format(baseQuery, sub);
+        return MessageFormat.format(baseQuery, sub);
+    }
+
+    private StudentData parseResultSet(ResultSet rs, List<FieldData> fieldData) throws SQLException {
         StudentData result = new StudentData();
         result.header = fieldData;
         List<RowValue> rowValues = new LinkedList<>();
-        try (PreparedStatement statement = this.connection.prepareStatement(fullQuery)) {
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()){
-                RowValue rowValue = new RowValue();
-                rowValue.userId = rs.getInt("system_user_id");
-                rowValue.name = rs.getString("name") + " " + rs.getString("surname");
-                for (FieldData i : fieldData) {
-                    rowValue.fields.put(i.id, parseResult(rs, i.id));
-                }
-                rowValues.add(rowValue);
+        while (rs.next()){
+            RowValue rowValue = new RowValue();
+            rowValue.userId = rs.getInt("system_user_id");
+            rowValue.name = rs.getString("name") + " " + rs.getString("surname");
+            for (FieldData i : fieldData) {
+                rowValue.fields.put(i.id, getField(rs, i.id));
             }
-            result.rows = rowValues;
-            return result;
-        } catch (Exception e) {
-            throw new DAOException(e);
+            rowValues.add(rowValue);
         }
+        result.rows = rowValues;
+        return result;
     }
 
-    private Object parseResult(ResultSet rs, int i) throws SQLException {
+    private Object getField(ResultSet rs, int i) throws SQLException {
         Object result;
         String fieldName = MessageFormat.format("field_{0}_list_text", i);
         if (rs.getObject(fieldName) != null){
