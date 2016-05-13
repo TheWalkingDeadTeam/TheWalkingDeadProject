7b6ca589f92e1a98536b586d5e35bf75be61d5d6
vdanchul <rangargst85@gmail.com>
2016-05-13 17:36:01 +0300

    IntervieweeTable row.

---
+++ src/main/java/ua/nc/entity/IntervieweeRow.java
@@ -0,0 +1,14 @@
+package ua.nc.entity;
+
+/**
+ * Created by Rangar on 13.05.2016.
+ */
+public class IntervieweeRow {
+    public Integer id;
+    public String name;
+    public String surname;
+    public String special_mark;
+    public String dev_score;
+    public String hr_score;
+    public Integer color;
+}
