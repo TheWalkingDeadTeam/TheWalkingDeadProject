d035d9e1bb7189ea64223ce6e9fc59503af35a6d
MaxxMorozov <morozovmaksim@email.ua>
2016-05-18 16:53:02 +0300

    Scheduler code refactoring

--- web/resources/js/changeRoles.js
+++ web/resources/js/changeRoles.js
@@ -2,23 +2,6 @@
  * Created by Max Morozov on 15.05.2016.
  */
 
-$(document).ready(function(){
-    var id = location.search.substr(1);
-    $.ajax({
-        type: 'get',
-        url: isNaN(id) ? '/getUser' : '/getUser/'+id,
-        dataType: 'json',
-        success: function(response){
-            $('#userName').text(response.name);
-            $('#userSurname').text(response.surname);
-            $('#userEmail').text(response.email);
-        },
-        error: function (jqXHR, exception) {
-            window.location.href = "/error"
-        }
-    })
-})
-
 $(document).ready(function () {
 
     $("#save_roles").click(function () {
