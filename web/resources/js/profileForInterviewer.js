b69b6defc6e5d38e740148c647cb4bf4a2c1fa32
sharko809 <sharko.daniel@gmail.com>
2016-05-25 14:30:19 +0300

    + fixes

--- web/resources/js/profileForInterviewer.js
+++ web/resources/js/profileForInterviewer.js
@@ -73,7 +73,7 @@ id="hr_score" class="label label-info label-as-badge pull-left">75</span></a></l
     var requestData;
     var id = location.search.substr(1);
     $(document).ready(function () {
-        $('#photo_img').attr('src','/getPhoto/'+id);
+        $('#photo_img').attr('src', '/getPhoto/' + id);
         $.ajax({
             type: 'get',
             url: "/profile/" + id,
@@ -91,12 +91,13 @@ id="hr_score" class="label label-info label-as-badge pull-left">75</span></a></l
                         dataType: 'json',
                         contentType: "application/json",
                         success: function (response) {
-                            if (response.viewLevel == 'restricted'){
+                            if (response.viewLevel == 'restricted') {
                                 $('#feedbacks')
                                     .addClass('alert alert-danger')
-                                    .html('It\'s too early to leave feedbacks');
-                            } else if (response.viewLevel == 'interviewing'){
-                                if(response.applicationExists == true && response.intervieweeExists == true) {
+                                    .html('It\'s too early to leave feedbacks')
+                                    .fadeIn();
+                            } else if (response.viewLevel == 'interviewing') {
+                                if (response.applicationExists == true && response.intervieweeExists == true) {
                                     if (response.restricted == false) {
                                         $('#feedbacks').html(div_form);
                                         var feedback;
@@ -105,7 +106,7 @@ id="hr_score" class="label label-info label-as-badge pull-left">75</span></a></l
                                         } else {
                                             feedback = response.hrFeedback;
                                         }
-                                        if (feedback != null){
+                                        if (feedback != null) {
                                             $('#feedback_score').val(feedback.score);
                                             $('#feedback_text').val(feedback.comment);
                                             $('#special_mark').val(response.specialMark);
@@ -124,21 +125,26 @@ id="hr_score" class="label label-info label-as-badge pull-left">75</span></a></l
                                                         },
                                                         specialMark: $('#special_mark').val()
                                                     }),
-                                                    success: function (response){
-                                                        if (response.length){
+                                                    success: function (response) {
+                                                        if (response.length) {
                                                             var errors_out = "";
                                                             for (var i in response) {
                                                                 errors_out += response[i].errorMessage + "</br>"
                                                             }
                                                             $('#save_message')
                                                                 .addClass('alert alert-danger')
-                                                                .html(errors_out);
+                                                                .html(errors_out)
+                                                                .fadeIn();
                                                         }
                                                         else {
                                                             $('#save_message')
                                                                 .removeClass()
                                                                 .addClass('alert alert-success')
-                                                                .html('Successfully saved');
+                                                                .html('Successfully saved')
+                                                                .fadeIn();
+                                                            setTimeout(function () {
+                                                                $("#save_message").fadeOut().empty();
+                                                            }, 3000);
                                                         }
                                                     },
                                                     error: function (jqXHR, exception) {
@@ -151,20 +157,23 @@ id="hr_score" class="label label-info label-as-badge pull-left">75</span></a></l
                                     } else {
                                         $('#feedbacks')
                                             .addClass('alert alert-danger')
-                                            .html('Feedback for this student has already been written by another interviewer');
+                                            .html('Feedback for this student has already been written by another interviewer')
+                                            .fadeIn();
                                     }
                                 } else {
                                     $('#feedbacks')
                                         .addClass('alert alert-danger')
-                                        .html('This studen\'t wasn\'t interviewed');
+                                        .html('This studen\'t wasn\'t interviewed')
+                                        .fadeIn();
                                 }
-                            } else if (response.viewLevel == 'after'){
-                                if(response.applicationExists == true && response.intervieweeExists == true) {
+                            } else if (response.viewLevel == 'after') {
+                                if (response.applicationExists == true && response.intervieweeExists == true) {
                                     $('#feedbacks').html(div_view);
                                 } else {
                                     $('#feedbacks')
                                         .addClass('alert alert-danger')
-                                        .html('This studen\'t wasn\'t interviewed');
+                                        .html('This studen\'t wasn\'t interviewed')
+                                        .fadeIn();
                                 }
                             }
                         },
@@ -176,7 +185,8 @@ id="hr_score" class="label label-info label-as-badge pull-left">75</span></a></l
                 } else {
                     $('#restrict_message')
                         .addClass('alert alert-danger')
-                        .html('This user doesn\'t have the application');
+                        .html('This user doesn\'t have the application')
+                        .fadeIn();
                 }
             },
             error: function (jqXHR, exception) {
@@ -187,28 +197,27 @@ id="hr_score" class="label label-info label-as-badge pull-left">75</span></a></l
     });
 
 
-
-    function typeSwitcher(item, i, divname){
-            $('<div id=\"block' + i + '\">').appendTo($(divname));
-            $('<label>').attr({for: item.id}).text(item.fieldName + ':  ').appendTo($('#block' + i));
-                switch (item.fieldType) {
-                case 'number':
-                case 'text':
-                case 'textarea':
-                case 'tel':
-                    $('<span>').text(item.values[0].value+'').appendTo($('#block' + i));
-                    break;
-                case 'select':
-                case 'checkbox':
-                case 'radio':
-                        var text = new Array;
-                        item.values.forEach(function(item_value){
-                            if (item_value.value == 'true'){
-                                text.push(item_value.fieldValueName);
-                            }
-                        })
-                        $('<span>').text(text.toString()).appendTo($('#block' + i));
-                        break;
-            }
+    function typeSwitcher(item, i, divname) {
+        $('<div id=\"block' + i + '\">').appendTo($(divname));
+        $('<label>').attr({for: item.id}).text(item.fieldName + ':  ').appendTo($('#block' + i));
+        switch (item.fieldType) {
+            case 'number':
+            case 'text':
+            case 'textarea':
+            case 'tel':
+                $('<span>').text(item.values[0].value + '').appendTo($('#block' + i));
+                break;
+            case 'select':
+            case 'checkbox':
+            case 'radio':
+                var text = new Array;
+                item.values.forEach(function (item_value) {
+                    if (item_value.value == 'true') {
+                        text.push(item_value.fieldValueName);
+                    }
+                })
+                $('<span>').text(text.toString()).appendTo($('#block' + i));
+                break;
+        }
     }
 })();
